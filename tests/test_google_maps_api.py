872c35236e4cf91138f86969c49940b692ef4840
dymekk <dzmitry@wolf.bet>
2023-11-15 15:37:06 +0100

    refactoring

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -1,10 +1,7 @@
+import allure
 from utils.my_api import GoogleMapsApi
 from utils.checking import Checking
 from utils.schemas import Schemas
-import allure
-
-
-"""Creating, Updating and Deleting new location"""
 
 
 @allure.epic("End-to-end test: Creating, Updating and Deleting new location")
@@ -12,71 +9,44 @@ class TestCreatePlace:
 
     @allure.title("Create new location by POST request")
     @allure.description("Creating new location")
-    def test_create_new_location(self):
+    def test_create_new_location(self, new_location):
         """Create new location by POST request"""
 
-        result_post = GoogleMapsApi.create_new_place()
-        result_post_body = result_post.json()
-        place_id = result_post_body.get("place_id")
-        Checking.check_status_code(result_post, 200)
-        Checking.check_json_keys_value(result_post, 'status', 'OK')
-        Checking.check_json_keys_value(result_post, 'scope', 'APP')
-        Checking.validation_field(result_post, Schemas.schema_create)
-
-        """Verify creating new location by GET request"""
-
-        result_get = GoogleMapsApi.get_new_place(place_id)
+        result_get = GoogleMapsApi.get_new_place(new_location)
         Checking.check_status_code(result_get, 200)
         Checking.validation_field(result_get, Schemas.schema_get_location)
-
         Checking.check_json_keys_value(result_get, 'address', '29, side layout, cohen 09')
 
     @allure.title("Updating new location by PUT request")
     @allure.description("Updating new location")
-    def test_update_new_location(self):
-        """Create new location by POST request"""
-
-        result_post = GoogleMapsApi.create_new_place()
-        result_post_body = result_post.json()
-        place_id = result_post_body.get("place_id")
-
+    def test_update_new_location(self, new_location):
         """Update new location address by PUT request"""
 
-        result_put = GoogleMapsApi.update_for_address_new_place(place_id)
+        result_put = GoogleMapsApi.update_for_address_new_place(new_location)
         Checking.check_status_code(result_put, 200)
         Checking.validation_field(result_put, Schemas.schema_msg)
         Checking.check_json_keys_value(result_put, 'msg', 'Address successfully updated')
 
         """Verify updating new location's address by GET request"""
 
-        result_get = GoogleMapsApi.get_new_place(place_id)
+        result_get = GoogleMapsApi.get_new_place(new_location)
         Checking.check_status_code(result_get, 200)
         Checking.check_json_keys_value(result_get, 'address', '100 Mickiewicz street, PL')
         Checking.validation_field(result_get, Schemas.schema_get_location)
 
     @allure.title("Deleting new location by DELETE request")
     @allure.description("Deleting new location")
-    def test_delete_new_location(self):
-        """Create new location by POST request"""
+    def test_delete_new_location(self, new_location):
+        """Delete new location by DELETE request"""
 
-        result_post = GoogleMapsApi.create_new_place()
-        result_post_body = result_post.json()
-        place_id = result_post_body.get("place_id")
-
-        """Update new location address by PUT request"""
-
-        GoogleMapsApi.update_for_address_new_place(place_id)
-
-        """ Delete new location by DELETE request"""
-
-        result_del = GoogleMapsApi.delete_new_place(place_id)
+        result_del = GoogleMapsApi.delete_new_place(new_location)
         Checking.check_status_code(result_del, 200)
         Checking.check_json_keys_value(result_del, 'status', 'OK')
         Checking.validation_field(result_del, Schemas.schema_delete)
 
         """Verify location deleted by GET request"""
 
-        result_get = GoogleMapsApi.get_new_place(place_id)
+        result_get = GoogleMapsApi.get_new_place(new_location)
         Checking.check_status_code(result_get, 404)
         Checking.check_json_keys_value(result_get, 'msg', "Get operation failed, looks like place_id  doesn't exists")
         Checking.validation_field(result_get, Schemas.schema_msg)
--- utils/logger.py
+++ utils/logger.py
@@ -1,3 +1,4 @@
+import allure
 import datetime
 import logging
 import os
@@ -5,6 +6,7 @@ import os
 
 class Logger:
     file_name = f"logs/log_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
     console_handler = logging.StreamHandler()
@@ -12,49 +14,31 @@ class Logger:
     logger.addHandler(console_handler)
 
     @classmethod
-    def write_log_to_file(cls, data: str):
-        with open(cls.file_name, 'a', encoding='utf=8') as logger_file:
-            logger_file.write(data)
+    def add_step(cls, message: str):
+        allure.attach(message, name="Step")
+        cls.write_log_to_file(message)
+        cls.logger.info(message)
 
     @classmethod
     def add_request(cls, url: str, method: str):
         test_name = os.environ.get('PYTEST_CURRENT_TEST')
-
-        data_to_add = f"\n-----\n"
-        data_to_add += f"Test: {test_name}\n"
-        data_to_add += f"Time: {str(datetime.datetime.now())}\n"
-        data_to_add += f"Request method: {method}\n"
-        data_to_add += f"Request URL: {url}\n"
-        data_to_add += f"\n"
-
-        cls.write_log_to_file(data_to_add)
-        Logger.logger.info(data_to_add)
+        message = f"\n-----\nTest: {test_name}\nTime: {str(datetime.datetime.now())}\nRequest method: {method}\nRequest URL: {url}\n\n"
+        cls.add_step(message)
 
     @classmethod
     def add_request_with_json(cls, url: str, method: str, json: str):
         test_name = os.environ.get('PYTEST_CURRENT_TEST')
-
-        data_to_add = f"\n-----\n"
-        data_to_add += f"Test: {test_name}\n"
-        data_to_add += f"Time: {str(datetime.datetime.now())}\n"
-        data_to_add += f"Request method: {method}\n"
-        data_to_add += f"Request URL: {url}\n"
-        data_to_add += f"Request body: {json}\n"
-        data_to_add += f"\n"
-
-        cls.write_log_to_file(data_to_add)
-        Logger.logger.info(data_to_add)
+        message = f"\n-----\nTest: {test_name}\nTime: {str(datetime.datetime.now())}\nRequest method: {method}\nRequest URL: {url}\nRequest body: {json}\n\n "
+        cls.add_step(message)
 
     @classmethod
     def add_response(cls, response):
         cookie_as_dict = dict(response.cookies)
         headers_as_dict = dict(response.headers)
+        message = f"Response code: {response.status_code}\nText response: {response.text}\nResponse headers: {headers_as_dict}\nResponse cookies: {cookie_as_dict}\n\n-----\n"
+        cls.add_step(message)
 
-        data_to_add = f"Response code: {response.status_code}\n"
-        data_to_add += f"Text response: {response.text}\n"
-        data_to_add += f"Response headers: {headers_as_dict}\n"
-        data_to_add += f"Response cookies: {cookie_as_dict}\n"
-        data_to_add += f"\n-----\n"
-
-        cls.write_log_to_file(data_to_add)
-        Logger.logger.info(data_to_add)
+    @classmethod
+    def write_log_to_file(cls, data: str):
+        with open(cls.file_name, 'a', encoding='utf-8') as logger_file:
+            logger_file.write(data)
