c4918e84fefc3bb8841fddfbcb23fda371b9dc08
dymekk <dzmitry@wolf.bet>
2023-11-15 15:34:47 +0100

    add conftestfile

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from utils.my_api import GoogleMapsApi
+
+
+@pytest.fixture
+def new_location():
+    """Fixture to create a new location and return its place_id."""
+    result_post = GoogleMapsApi.create_new_place()
+    result_post_body = result_post.json()
+    place_id = result_post_body.get("place_id")
+    return place_id
